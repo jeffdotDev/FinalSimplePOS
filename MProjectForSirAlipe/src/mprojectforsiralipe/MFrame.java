6dca346098a207c773677ce754d06a7494e98d90
jeff <147962351+jeffdotDev@users.noreply.github.com>
2024-09-28 21:04:51 +0800

    Add files via upload

---
+++ MProjectForSirAlipe/src/mprojectforsiralipe/MFrame.java
@@ -0,0 +1,15 @@
+
+package mprojectforsiralipe;
+
+
+public class MFrame {
+    public static void main(String[] args){
+        PosView pv = new PosView();
+        pv.setVisible(true);
+        new PosController(pv);
+        
+        
+        
+    }
+    
+}
